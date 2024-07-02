226864462cc026769555e5da103bce1a5d738278
Nikita1013 <shindenikita646@gmail.com>
2024-07-02 23:17:01 +0530

    commit-Day-16

---
+++ Day-16/another_module.py
@@ -0,0 +1,17 @@
+another_variable =12
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("Blueviolet")
+timmy.fd(100)
+timmy.left(90)
+timmy.forward(10)
+timmy.backward(100)
+timmy.circle(90)
+
+
+my_screen =  Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
